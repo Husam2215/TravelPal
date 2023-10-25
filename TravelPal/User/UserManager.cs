8852f029c1373a14b8641ad31dbe99662c64150d
Husam2215 <Husam.emrulovski1@gmail.com>
2023-10-25 10:22:17 +0200

    Initial commit

---
+++ TravelPal/User/UserManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TravelPal.User
+{
+    internal static class UserManager
+    {
+        static List<IUser> Users = new()
+        {
+
+        }
+    }
+}
